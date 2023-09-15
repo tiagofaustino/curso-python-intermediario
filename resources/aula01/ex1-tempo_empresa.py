834c97edd82bc5af887005bf483f079ccabd6ec8
Tiago Faustino <tfaustino@gmail.com>
2023-09-15 19:53:59 -0300

    Retirada do caminho completo para criação dos arquivos

--- resources/aula01/ex1-tempo_empresa.py
+++ resources/aula01/ex1-tempo_empresa.py
@@ -4,7 +4,9 @@
 # E calcular a quantidade de anos em que trabalharam na empresa
 
 #inicia o arquivo em modo de escrita
-url_ex1 = 'C:\\Projects\\Estudos\\accenture\\python-intermediario\\curso-python-intermediario\\resources\\aula01-exercicio-anos-empresa.txt'
+import os
+current_dir = os.path.dirname(os.path.realpath('__file__')) #pega o diretório corrente do arquivo
+url_ex1 = os.path.join(current_dir, 'exercicio-anos-empresa.txt') #junta as informações de path do arquivo
 arquivo_ex1 = open(url_ex1, "w")
 
 #cria as váriaveis
@@ -21,6 +23,7 @@ leitura_ex1 = open(url_ex1, 'r')
 valor_ano_entrada = int(leitura_ex1.readline().replace('\n', ''))
 valor_ano_atual = int(leitura_ex1.readline())
 leitura_ex1.close()
+
 
 #calcula o tempo de empresa
 tempo_empresa = valor_ano_atual - valor_ano_entrada
