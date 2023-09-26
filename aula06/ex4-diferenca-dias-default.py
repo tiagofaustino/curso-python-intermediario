10bc3b7100a0061312b1c401c72dc8700811685e
Tiago Faustino <tfaustino@gmail.com>
2023-09-25 22:43:10 -0300

    Commits da aula 6 ex4

--- aula06/ex4-diferenca-dias-default.py
+++ aula06/ex4-diferenca-dias-default.py
@@ -7,6 +7,7 @@
 import calendar
 from datetime import datetime
 
+#forma 1 de se fazer
 def diferenca_dias(ano, mes=12): 
     dia = calendar.monthrange(ano, mes)[1]
     return diferenca_dias1(ano, mes, dia)
@@ -14,17 +15,33 @@ def diferenca_dias(ano, mes=12):
 def diferenca_dias1(ano, mes, dia):
     return calcula_diferenca(datetime.now(), datetime(ano, mes, dia))
 
+#forma 2 de se fazer
+def diferenca_dias_sol2(ano, mes, dia):
+    if mes is None or mes <= 0:
+        mes = 12
+    if dia is None or dia <= 0:
+        dia = calendar.monthrange(ano, mes)[1]
+    return calcula_diferenca(datetime.now(), datetime(ano, mes, dia))
+
 def calcula_diferenca(date_time1, date_time2):
     diff = date_time1 - date_time2
     return diff.days
 
 print(diferenca_dias(2021, 2))
+print(diferenca_dias_sol2(2021, 2, 0))
+
 print(diferenca_dias(2021))
+print(diferenca_dias_sol2(2021, 0, 0))
+
 print(diferenca_dias1(2021,2,22))
+print(diferenca_dias_sol2(2021,2,22))
 
 #Resultado
 '''
 939
+939
 633
+633
+945
 945
 '''
